3dea51b2bb3d98a7f4ad39f361bf7ccd0a72f795
LucasOSilvaTaak <lucas.silva@taak.com.br>
2026-01-07 15:17:09 -0300

    Feat: dara inicial modificada

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,7 +1,8 @@
 const { SchedulerLoader } = require("./Schedule/schedule.js");
+import {dayController} from './Schedule/dayController';
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+    dayController.startDate();
     console.log("Page Loaded and Schedule Initialized");
     SchedulerLoader.loadSchedule("2024-07-11");
 
